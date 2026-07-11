58843c90116464bf3d40073065ca3f53038fa983
syachi-hoko-maru <realmadrid.dmplay.s@gmail.com>
2026-07-11 16:18:37 +0900

    ウェブサイトを追加

--- app/data/circleDetails.ts
+++ app/data/circleDetails.ts
@@ -11,27 +11,32 @@ export const circleDetails: Record<string, CircleDetail> = {
       {
         name: "【先行体験版】プラネスデオメノス 巡涯学派星辰祈譚（Booth）",
         url: "https://planes.booth.pm/items/8594148",
-        description: "イベント限定の先行体験版を無償頒布。天体と信仰をテーマにした同人サプリメント。この旅の涯に、きみは何を願い祈る？",
+        description:
+          "イベント限定の先行体験版を無償頒布。天体と信仰をテーマにした同人サプリメント。この旅の涯に、きみは何を願い祈る？",
       },
       {
         name: "【電子版】プラネスセオルギア 巡涯学派魔法大系（Booth）",
         url: "https://planes.booth.pm/items/7268434",
-        description: "ラクシアを旅する、魔法研究者集団「巡涯学派」。彼らの研究成果を紹介するオリジナルデータ集。",
+        description:
+          "ラクシアを旅する、魔法研究者集団「巡涯学派」。彼らの研究成果を紹介するオリジナルデータ集。",
       },
       {
         name: "【紙書籍】プラネスセオルギア 巡涯学派魔法大系（Booth）",
         url: "https://planes.booth.pm/items/7268444",
-        description: "ラクシアを旅する、魔法研究者集団「巡涯学派」。彼らの研究成果を紹介するオリジナルデータ集。",
+        description:
+          "ラクシアを旅する、魔法研究者集団「巡涯学派」。彼らの研究成果を紹介するオリジナルデータ集。",
       },
       {
         name: "ソード・ワールド2.5シンボルフォント プラネスセミオチカ 巡涯学派記号目録（Booth）",
         url: "https://planes.booth.pm/items/7847999",
-        description: "ソード・ワールド2.5作品内で使用されている記号やシンボルなどを、アレンジを加えつつまとめたフォントデータ。",
+        description:
+          "ソード・ワールド2.5作品内で使用されている記号やシンボルなどを、アレンジを加えつつまとめたフォントデータ。",
       },
       {
         name: "ソード・ワールド2.5 追加戦闘ルール ヘックスマス戦闘＋汎用ヘックスマップ素材",
         url: "https://planes.booth.pm/items/8084955",
-        description: "熟練戦闘をベースに、戦略性の高さを残しつつ、座標の管理の簡略化を図った戦闘ルール。ココフォリア向けの画像素材もあります。",
+        description:
+          "熟練戦闘をベースに、戦略性の高さを残しつつ、座標の管理の簡略化を図った戦闘ルール。ココフォリア向けの画像素材もあります。",
       },
     ],
     xAccounts: ["organon_torah"],
@@ -56,7 +61,8 @@ export const circleDetails: Record<string, CircleDetail> = {
       {
         name: "クレメリア情報局",
         url: "https://dagonunder6feet.booth.pm/",
-        description: "創作大陸【クレメリア】を舞台にしたシナリオ・データ集を頒布しています。また、ドット絵などの頒布もやっています！",
+        description:
+          "創作大陸【クレメリア】を舞台にしたシナリオ・データ集を頒布しています。また、ドット絵などの頒布もやっています！",
       },
     ],
     xAccounts: ["DagonUnder6Feet"],
@@ -66,27 +72,32 @@ export const circleDetails: Record<string, CircleDetail> = {
       {
         name: "データ集『バウンティ・モンスターズ』",
         url: "https://momijibakufu.booth.pm/items/8577520",
-        description: "オリジナルのボス級魔物データ集です。  全15種の魔物を、形態違い・随伴データ込みで計19データ収録。",
+        description:
+          "オリジナルのボス級魔物データ集です。  全15種の魔物を、形態違い・随伴データ込みで計19データ収録。",
       },
       {
         name: "シナリオ集『sw2.5シナリオ集vol.1』",
         url: "https://momijibakufu.booth.pm/items/7677618",
-        description: "高レベル帯のキャンペーンと、中レベル帯の単発シナリオを収録しています。",
+        description:
+          "高レベル帯のキャンペーンと、中レベル帯の単発シナリオを収録しています。",
       },
       {
         name: "シナリオ集『sw2.5シナリオ集vol.2』",
         url: "https://momijibakufu.booth.pm/items/7677824",
-        description: "中レベル帯のキャンペーンと、中レベル帯の単発シナリオを収録しています。",
+        description:
+          "中レベル帯のキャンペーンと、中レベル帯の単発シナリオを収録しています。",
       },
       {
         name: "シナリオ集『sw2.5シナリオ集vol.3』",
         url: "https://momijibakufu.booth.pm/items/7677930",
-        description: "中レベル帯の単発シナリオ2本と、初期作成蛮族用シナリオを収録しています。",
+        description:
+          "中レベル帯の単発シナリオ2本と、初期作成蛮族用シナリオを収録しています。",
       },
       {
         name: "シナリオ集『sw2.5シナリオ集vol.4』",
         url: "https://momijibakufu.booth.pm/items/7678619",
-        description: "中レベル帯の単発シナリオ2本と、初期作成蛮族用シナリオを収録しています。",
+        description:
+          "中レベル帯の単発シナリオ2本と、初期作成蛮族用シナリオを収録しています。",
       },
     ],
     xAccounts: ["momiji_bakufu"],
@@ -96,7 +107,8 @@ export const circleDetails: Record<string, CircleDetail> = {
       {
         name: "想起の国ノストロス",
         url: "https://kalia.booth.pm/items/8495920",
-        description: "魔動機文明時代の息吹が色濃く残るオリジナルの小国の地方誌です。追加データの他に魔動機を作って戦うミニゲームルールを収録",
+        description:
+          "魔動機文明時代の息吹が色濃く残るオリジナルの小国の地方誌です。追加データの他に魔動機を作って戦うミニゲームルールを収録",
       },
     ],
     xAccounts: ["TRPG_CCrown"],
@@ -116,7 +128,8 @@ export const circleDetails: Record<string, CircleDetail> = {
       {
         name: "studio SISTER(Booth)",
         url: "https://jorge.booth.pm/",
-        description: "キャンペーンシナリオ『雛ドラゴンの育て方』や、流派追加サプリメント『Wonder Arts Record』を頒布中です。",
+        description:
+          "キャンペーンシナリオ『雛ドラゴンの育て方』や、流派追加サプリメント『Wonder Arts Record』を頒布中です。",
       },
       {
         name: "studio SISTER HP",
@@ -126,7 +139,8 @@ export const circleDetails: Record<string, CircleDetail> = {
       {
         name: "Wonder Arts Record特設サイト",
         url: "https://studio-sister.com/wonder-arts-record/",
-        description: "流派追加サプリメントシリーズ『Woder Arts Record』の特設サイトです。",
+        description:
+          "流派追加サプリメントシリーズ『Woder Arts Record』の特設サイトです。",
       },
     ],
     xAccounts: ["Jorge_stSISTER"],
@@ -140,7 +154,8 @@ export const circleDetails: Record<string, CircleDetail> = {
       {
         name: "わっかのTRPG部屋（BOOTH）",
         url: "https://wakkacha.booth.pm/",
-        description: "TRPG（SW2.5）のシナリオ、ココフォリア用オンセ素材などを頒布しています。",
+        description:
+          "TRPG（SW2.5）のシナリオ、ココフォリア用オンセ素材などを頒布しています。",
       },
       {
         name: "わっかのシナリオ（TALTO）",
@@ -155,7 +170,8 @@ export const circleDetails: Record<string, CircleDetail> = {
       {
         name: "鹿川精肉店（BOOTH）",
         url: "https://shikagawaseiniku.booth.pm/item_lists/8GXT3kd2",
-        description: "ソドワファンフェスに出す物をまとめた商品ページです！ブルライト地方のマップだったりうさぎ詰めの更新を行ったりします！",
+        description:
+          "ソドワファンフェスに出す物をまとめた商品ページです！ブルライト地方のマップだったりうさぎ詰めの更新を行ったりします！",
       },
     ],
     xAccounts: ["kainiku019"],
@@ -165,7 +181,8 @@ export const circleDetails: Record<string, CircleDetail> = {
       {
         name: "【電子版】SW2.5同人サプリメント 泡沫世界アーティカタパウシス SPLL:E111083(Booth)",
         url: "https://okorudesikasi.booth.pm/items/8488128",
-        description: "古代地中海風のオリジナルの泡沫世界、『アーティカタパウシス』を舞台にした同人サプリメントを頒布中！",
+        description:
+          "古代地中海風のオリジナルの泡沫世界、『アーティカタパウシス』を舞台にした同人サプリメントを頒布中！",
       },
     ],
     xAccounts: ["SWyattemitai"],
@@ -175,27 +192,32 @@ export const circleDetails: Record<string, CircleDetail> = {
       {
         name: "シナリオ集「冒険者の旅路」",
         url: "https://booth.pm/ja/items/4995890",
-        description: "基本ルールブックⅠ・Ⅱ・Ⅲを使用して遊べるシナリオ集です。 6本のシナリオとキャンペーン(12L帯40ページ程)を収録。",
+        description:
+          "基本ルールブックⅠ・Ⅱ・Ⅲを使用して遊べるシナリオ集です。 6本のシナリオとキャンペーン(12L帯40ページ程)を収録。",
       },
       {
         name: "データ集『Dr.マーキュリーの研究日誌』",
         url: "https://fantasiticden.booth.pm/items/6093526",
-        description: "オリジナル薬草や魔法薬を紹介するデータ集。18種の薬草と70種の魔法薬を収録しています。薬草にはイラストも。",
+        description:
+          "オリジナル薬草や魔法薬を紹介するデータ集。18種の薬草と70種の魔法薬を収録しています。薬草にはイラストも。",
       },
       {
         name: "シナリオ『砂漠に眠りし秘宝』",
         url: "",
-        description: "砂漠を舞台にしたシナリオ集です。 2～5Lv帯のキャンペーンが1つ、単発で3つのシナリオが収録されています。",
+        description:
+          "砂漠を舞台にしたシナリオ集です。 2～5Lv帯のキャンペーンが1つ、単発で3つのシナリオが収録されています。",
       },
       {
         name: "1時間シナリオ集『ブルライト狂想曲』",
         url: "https://fantasiticden.booth.pm/items/6937544",
-        description: "ブルライト地方を舞台に、1時間の短いシナリオを10本収録しています。PL数も2～3人を主にしており気軽に遊べる作品です。",
+        description:
+          "ブルライト地方を舞台に、1時間の短いシナリオを10本収録しています。PL数も2～3人を主にしており気軽に遊べる作品です。",
       },
       {
         name: "小説「放浪神の栞～ロクの冒険～」",
         url: "https://fantasiticden.booth.pm/items/7533913",
-        description: "「冒険」をテーマにした、ソード・ワールド2.5の短編小説集です。 1万～2万字の読みごたえある物語を、全6作品収録。",
+        description:
+          "「冒険」をテーマにした、ソード・ワールド2.5の短編小説集です。 1万～2万字の読みごたえある物語を、全6作品収録。",
       },
     ],
     xAccounts: ["fantasticden369"],
@@ -215,7 +237,8 @@ export const circleDetails: Record<string, CircleDetail> = {
       {
         name: "ナナイロダイス(Booth)",
         url: "https://nanairodaisu.booth.pm/",
-        description: "GM用新規罠・冒険者用新アイテム・シナリオ入り！「罠」にフォーカスしたオリジナルサプリメント頒布中です！",
+        description:
+          "GM用新規罠・冒険者用新アイテム・シナリオ入り！「罠」にフォーカスしたオリジナルサプリメント頒布中です！",
       },
     ],
     xAccounts: ["nanairodice"],
@@ -225,12 +248,14 @@ export const circleDetails: Record<string, CircleDetail> = {
       {
         name: "【BOOTH】こむぎもち屋台",
         url: "https://shira-mizu.booth.pm/",
-        description: "PC・NPCに使える立ち絵素材を頒布しています。無料版・差分付き有料版があります。",
+        description:
+          "PC・NPCに使える立ち絵素材を頒布しています。無料版・差分付き有料版があります。",
       },
       {
         name: "【TALTO】こむぎもち",
         url: "https://talto.cc/users/fylmSFaC8QNM9a1kQVjqahlEzE52",
-        description: "無料シナリオを掲載しています。書き下ろし新シナリオもあります。",
+        description:
+          "無料シナリオを掲載しています。書き下ろし新シナリオもあります。",
       },
     ],
     xAccounts: ["Shira_mizu"],
@@ -240,7 +265,8 @@ export const circleDetails: Record<string, CircleDetail> = {
       {
         name: "Thyme（Booth）",
         url: "https://thymetostart0.booth.pm/",
-        description: "ソードワールド2.5(SW2.5)の「ストーリー型サプリメント」と「タワーディフェンス型ボードゲーム」を頒布しています。",
+        description:
+          "ソードワールド2.5(SW2.5)の「ストーリー型サプリメント」と「タワーディフェンス型ボードゲーム」を頒布しています。",
       },
     ],
     xAccounts: ["thymetostart0"],
@@ -260,7 +286,8 @@ export const circleDetails: Record<string, CircleDetail> = {
       {
         name: "人造魔剣(アルタードウェポン)（Booth）",
         url: "https://ruromin.booth.pm/items/8596113",
-        description: "PLが好きに数値を弄れる「自分だけの1本を作れる」魔剣本です。",
+        description:
+          "PLが好きに数値を弄れる「自分だけの1本を作れる」魔剣本です。",
       },
       {
         name: "ユランアウルク博物誌（Booth）",
@@ -300,22 +327,26 @@ export const circleDetails: Record<string, CircleDetail> = {
       {
         name: "【SW2.5】辺境ギルド天守亭の掲示板 vol.1【シナリオアンソロジー】（Booth）",
         url: "https://booth.pm/ja/items/7816909",
-        description: "辺境の島を舞台にしたシナリオアンソロジーです。初心者向け(レベル2～5)、短時間のシナリオが3本収録されています。",
+        description:
+          "辺境の島を舞台にしたシナリオアンソロジーです。初心者向け(レベル2～5)、短時間のシナリオが3本収録されています。",
       },
       {
         name: "【SW2.5】辺境ギルド天守亭の掲示板 vol.2【シナリオアンソロジー】（Booth）",
         url: "https://insiderain.booth.pm/items/8598554",
-        description: "辺境の島を舞台にした、ソード・ワールド2.5のアンソロジー第2弾です。4本のシナリオを収録しています。",
+        description:
+          "辺境の島を舞台にした、ソード・ワールド2.5のアンソロジー第2弾です。4本のシナリオを収録しています。",
       },
       {
         name: "【SW2.5】ゆる～い魔物立ち絵素材（Booth）",
         url: "https://insiderain.booth.pm/items/8598063",
-        description: "ゆる～い見た目の魔物の立ち絵素材です。オンラインセッションなどにご利用ください。",
+        description:
+          "ゆる～い見た目の魔物の立ち絵素材です。オンラインセッションなどにご利用ください。",
       },
       {
         name: "【SW2.5】基本ルルブ風イラスト【素材】（Booth）",
         url: "https://insiderain.booth.pm/items/8583546",
-        description: "「ソード・ワールド2.5　基本ルールブック1」の書影を模して描いた素材です。オンラインセッションなどにご利用ください。",
+        description:
+          "「ソード・ワールド2.5　基本ルールブック1」の書影を模して描いた素材です。オンラインセッションなどにご利用ください。",
       },
     ],
     xAccounts: ["InsideRain_TRPG"],
@@ -335,7 +366,8 @@ export const circleDetails: Record<string, CircleDetail> = {
       {
         name: "イモータル・テイル ～ヴロチュカ英雄記～",
         url: "https://togakiya.booth.pm/",
-        description: "ランドール地方を舞台にした学園ものキャンペーンシナリオを頒布しています。前後編あわせて全7話です。",
+        description:
+          "ランドール地方を舞台にした学園ものキャンペーンシナリオを頒布しています。前後編あわせて全7話です。",
       },
     ],
     xAccounts: ["togakiya"],
@@ -360,22 +392,30 @@ export const circleDetails: Record<string, CircleDetail> = {
       {
         name: "しゃちほこの尾びれ亭の同人誌（Booth）",
         url: "https://syachihokomaru.booth.pm/item_lists/nJgTVYPX",
-        description: "独自の種族/技能/流派/魔物等を掲載した、オリジナルの舞台で、和風な冒険ができる同人サプリメントなどを頒布しています",
+        description:
+          "独自の種族/技能/流派/魔物等を掲載した、オリジナルの舞台で、和風な冒険ができる同人サプリメントなどを頒布しています",
       },
       {
         name: "しゃちほこ丸のシナリオ（TALTO）",
         url: "https://talto.cc/users/DVUC9LjUviekGgXrbqXaJWAcLCm1",
-        description: "初心者さんと初心者GMさんにおすすめのシナリオや、GMレスで遊べるシナリオなどを頒布しています",
+        description:
+          "初心者さんと初心者GMさんにおすすめのシナリオや、GMレスで遊べるシナリオなどを頒布しています",
       },
       {
         name: "初心者さん向けルールブック副読本『いざ、ファンタジーな世界へ』",
         url: "https://syachihokomaru.booth.pm/items/7536991",
-        description: "SW2.5の未経験者さん、初心者さんが気軽にルールブックを読みつつ遊んで慣れられる副読本です。GMレスシナリオ収録。",
+        description:
+          "SW2.5の未経験者さん、初心者さんが気軽にルールブックを読みつつ遊んで慣れられる副読本です。GMレスシナリオ収録。",
       },
       {
         name: "しゃちほこの尾びれ亭のアパレル（Booth）",
         url: "https://syachihokomaru.booth.pm/item_lists/rBkTlPKW",
         description: "ソドワ好きの方のためのシャツなどを頒布しています！",
+      },
+      {
+        name: "ウェブサイト「しゃちほこの尾びれ亭」",
+        url: "https://trpg.syachi.work",
+        description: "ソドワの情報などを発信しています！",
       },
     ],
     xAccounts: ["syachi_hoko_trp"],
